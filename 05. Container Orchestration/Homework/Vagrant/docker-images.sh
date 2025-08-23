549e9a06d8319efb7e704060593c2120eb2d31e7
simonen <si.petrov@yahoo.com>
2025-08-23 18:40:37 +0300

    update: add conditional for git directory

--- 05. Container Orchestration/Homework/Vagrant/docker-images.sh
+++ 05. Container Orchestration/Homework/Vagrant/docker-images.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-git clone https://github.com/simonen/bgapp-homework.git
+if [ ! -d "/path/to/dir" ]; then
+  echo "Directory does not exist, running command..."
+  git clone https://github.com/simonen/bgapp-homework.git
+else
+  echo "Directory exists, skipping."
+fi
 
 cd bgapp-homework
-echo "DB_ROOT_PASSWORD=<db_root_pass>" > .env
+echo "DB_ROOT_PASSWORD=12345" > .env
 
 docker build -t web-image -f Dockerfile.web .
 docker build -t db-image -f Dockerfile.db .
