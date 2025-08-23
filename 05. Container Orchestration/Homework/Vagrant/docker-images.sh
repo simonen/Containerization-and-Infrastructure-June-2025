afdbce4d19ef5cd7cfb6e32df70f76dac62e0e42
simonen <si.petrov@yahoo.com>
2025-08-23 17:30:29 +0300

    fix: removed swarm operations

--- 05. Container Orchestration/Homework/Vagrant/docker-images.sh
+++ 05. Container Orchestration/Homework/Vagrant/docker-images.sh
@@ -3,10 +3,10 @@
 git clone https://github.com/simonen/bgapp-homework.git
 
 cd bgapp-homework
-echo "DB_ROOT_PASSWORD=12345" > .env
+echo "DB_ROOT_PASSWORD=<db_root_pass>" > .env
 
 docker build -t web-image -f Dockerfile.web .
 docker build -t db-image -f Dockerfile.db .
 docker build -t php-image -f Dockerfile.fpm .
 
-docker compose up -d
+#docker secret create db_root_pass .env
