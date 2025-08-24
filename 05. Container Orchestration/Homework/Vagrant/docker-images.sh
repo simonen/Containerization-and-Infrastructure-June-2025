957e800b12c0bdd8ecdf308bf333a02052b364f1
simonen <si.petrov@yahoo.com>
2025-08-24 21:40:56 +0300

    update: test

--- 05. Container Orchestration/Homework/Vagrant/docker-images.sh
+++ 05. Container Orchestration/Homework/Vagrant/docker-images.sh
@@ -5,13 +5,21 @@ if [ -d "/home/vagrant/bgapp-homework" ]; then
   sudo rm -rf /home/vagrant/bgapp-homework
 fi
 
-echo "Cloning repository..."
-git clone https://github.com/simonen/bgapp-homework.git
+#echo "Cloning repository..."
+#git clone https://github.com/simonen/bgapp-homework.git
+
+mkdir -v /home/vagrant/bgapp-homework
+cp -r -v /app/. /home/vagrant/bgapp-homework
 
 chown -R vagrant:vagrant /home/vagrant/bgapp-homework
 cd bgapp-homework
 
-sudo rm -rf .git
+#sudo rm -rf .git
+
+# Stop and delete container and images
+
+docker stop $(docker ps -q) || true
+docker rmi -f $(docker images -q) || true
 
 docker build -t web-image -f Dockerfile.web .
 docker build -t db-image -f Dockerfile.db .
--- 05. Container Orchestration/Homework/Vagrant/docker-setup.sh
+++ 05. Container Orchestration/Homework/Vagrant/docker-setup.sh
@@ -5,11 +5,11 @@ dnf -y install dnf-plugins-core
 dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
 
 echo "* Install Docker ..."
-dnf install -y app-ce app-ce-cli containerd.io app-buildx-plugin app-compose-plugin
+dnf install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 echo "* Enable and start Docker ..."
-systemctl enable app
-systemctl start app
+systemctl enable docker
+systemctl start docker
 
 echo "* Add vagrant user to docker group ..."
-usermod -aG app vagrant
+usermod -aG docker vagrant
